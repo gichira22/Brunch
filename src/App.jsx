296495749ec7b918396e8f693c67335ea25e4b97
Virginia Gichira <gichiravirginia@gmail.com>
2024-08-10 09:28:28 +0300

    Commit files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-red-700 text-white text-center py-4">Home</h1>
+    </div>
+  );
+};
+
+export default App;
